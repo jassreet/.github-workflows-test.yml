4cedba7eca7cd5156cac21d5a7fba8ee7859869d
Jaspreet Singh <jaspreetsingh9102000@gmail.com>
2024-12-07 00:35:57 -0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,11 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument(); // Passes if "learn react" is present
+});
+
+test('intentional failure test', () => {
+  render(<App />);
+  const nonexistentElement = screen.getByText(/this text does not exist/i);
+  expect(nonexistentElement).toBeInTheDocument(); // This will fail
 });
